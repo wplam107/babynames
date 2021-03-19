5ce93bdb312ebbcaa117ae69dfaf3bf2db6277a5
Wayne <w.p.lam107@gmail.com>
2021-03-19 18:33:16 -0400

    pre-alpha app

--- app/app.py
+++ app/app.py
@@ -1,7 +1,10 @@
+import os
 import dash_core_components as dcc
 import dash_html_components as html
 import dash
 from dash.dependencies import Input, Output
+
+import plotly.express as px
 
 import pandas as pd
 
@@ -10,12 +13,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from models import Base, Estimate, NameJSON
-from config import DATABASE_URI
+from cloud_config import DATABASE_URI
 
 import us
 
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
-engine = create_engine(DATABASE_URI, executemany_mode='batch')
+# DATABASE_URL = os.environ['DATABASE_URL']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+engine = create_engine(DATABASE_URI)
 Session = sessionmaker(bind=engine)
 s = Session()
 
@@ -30,28 +34,84 @@ def possible_names(name):
     
     return results
 
+def get_name(name):
+    q = s.query(NameJSON).\
+    filter(NameJSON.name == name).\
+    all()
+
+    female = pd.DataFrame(q[0].data['Female']).fillna(0).reset_index().rename(columns={'index': 'year'})
+    male = pd.DataFrame(q[0].data['Male']).fillna(0).reset_index().rename(columns={'index': 'year'})
+
+    female = female.melt(
+        id_vars='year',
+        value_vars=[ col for col in female.columns if col != 'year' ],
+        var_name='state',
+        value_name='births')
+    female['gender'] = 'Female'
+    male = male.melt(
+        id_vars='year',
+        value_vars=[ col for col in male.columns if col != 'year' ],
+        var_name='state',
+        value_name='births')
+    male['gender'] = 'Male'
+
+    df = pd.concat([female, male])
+    
+    return df
+
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+initial_fig = px.line(
+    range_x=[1960, 2019],
+    range_y=[0, 1000]
+)
 
 app.layout = html.Div(
     [
         html.Br(),
-        dcc.Input(id="input1", type="text", placeholder="Baby Name"),
-        html.Div(id="output"),
+        dcc.Dropdown(id="name-dropdown"),
+        html.Br(),
+        dcc.Graph(
+            id="name-plot",
+            figure=initial_fig,
+        ),
     ]
 )
 
 
 @app.callback(
-    Output("output", "children"),
-    Input("input1", "value"),
+    Output("name-dropdown", "options"),
+    Input("name-dropdown", "search_value")
 )
-def update_output(input1):
-    if input1 == 'Names in Database:':
-        return None
-    else:
-        names = ', '.join(possible_names(input1))
-        return 'Names in Database: {}'.format(names)
+def update_dropdown(search_value):
+    if not search_value:
+        raise dash.exceptions.PreventUpdate
+    return [ {'label': v, 'value': v} for v in possible_names(search_value) ]
+
+@app.callback(
+    Output("name-plot", "figure"),
+    Input("name-dropdown", "value")
+)
+def update_graph(value):
+    if not value:
+        raise dash.exceptions.PreventUpdate
+
+    if value:
+        df = get_name(value)
+        groups = ['year', 'gender']
+        fig = px.line(
+            df.groupby(groups)['births'].sum().reset_index(),
+            x='year',
+            y='births',
+            line_group='gender',
+            color='gender',
+            color_discrete_map={'Female': 'red', 'Male': 'blue'}
+        )
+
+    return fig
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run_server(debug=True)
